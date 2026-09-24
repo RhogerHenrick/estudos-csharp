cc1344a3f59744122025bba46d3c8c505efd334f
Rhoger Henrick <contato.rhogerh@gmail.com>
2026-09-24 04:24:06 -0300

    Adicionar primeiro projeto C#

---
+++ 01-PrimeiroPrograma/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Insira sua nota: ");
+int nota = Convert.ToInt32(Console.ReadLine());
+if (nota < 4)
+{
+    Console.WriteLine("reprovado");
+}
+else if (nota == 5 || nota == 6)
+{
+    Console.WriteLine("recuperando");
+}
+else if ( nota >= 7 && nota <= 10 )
+{
+    Console.WriteLine("Aprovado");
+}
+
+else if ( nota >= 11 )
+{
+    Console.WriteLine("Nota inválida");
+}
