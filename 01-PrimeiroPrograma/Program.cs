59c7dc20a39a66a0235ce8983f8386be0a083d8a
Rhoger Henrick <contato.rhogerh@gmail.com>
2026-09-24 04:27:56 -0300

    Corrige validacao de notas

--- 01-PrimeiroPrograma/Program.cs
+++ 01-PrimeiroPrograma/Program.cs
@@ -1,19 +1,19 @@
 ﻿Console.Write("Insira sua nota: ");
 int nota = Convert.ToInt32(Console.ReadLine());
-if (nota < 4)
+
+if (nota  < 0 || nota > 10)
+{
+    Console.WriteLine("Nota inválida");
+}
+if (nota <= 4)
 {
     Console.WriteLine("reprovado");
 }
-else if (nota == 5 || nota == 6)
+else if (nota <=6)
 {
     Console.WriteLine("recuperando");
 }
-else if ( nota >= 7 && nota <= 10 )
+else
 {
     Console.WriteLine("Aprovado");
-}
-
-else if ( nota >= 11 )
-{
-    Console.WriteLine("Nota inválida");
 }
